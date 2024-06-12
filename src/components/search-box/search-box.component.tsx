84a11d914606f8fb6af7643f8f861d40b44632c8
Cornel Mihaescu <cornu_bv@yahoo.com>
2024-06-12 11:01:30 +0300

    updated npm

--- src/components/search-box/search-box.component.tsx
+++ src/components/search-box/search-box.component.tsx
@@ -1,10 +1,10 @@
-import type { ChangeEventHandler } from 'react';
+import { ChangeEvent } from 'react';
 import './search-box.styles.css';
 
 type SearchBoxProps = {
 className:string;
 placeholder?:string;
-onChangeHandler:ChangeEventHandler<HTMLInputElement>;
+onChangeHandler:(e:ChangeEvent<HTMLInputElement>) => void
 }
 
 const SearchBox = ({ 
